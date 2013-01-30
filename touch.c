01322999497f41dce4ad6e6f89ed16bf936c669f
Matt Baker <matt.baker@mbientlab.com>
2013-01-30 07:00:22 +0000

    Implemented most of the touch functionality. Touch uses the ADC, Timer1, and the CTMU. Need to write code to handle sample averaging, and relay touch information to the application.

---
+++ touch.c
@@ -0,0 +1,167 @@
+#include <xc.h>
+
+#include "touch.h"
+
+/** Struct that represents a touch channel. */
+typedef struct {
+    unsigned port:2;
+    unsigned pindex:4;
+    unsigned amux:2;
+    unsigned aindex:4;
+} touch_channel;
+
+enum cmat {CMAT_PORT, CMAT_POS, CMAT_AMUX, CMAT_AN};
+
+/** Const array that contains touch channel->peripheral mapping information. */
+const touch_channel cmatrix[TOUCH_CHANNEL_COUNT] = {{1, 2, 0, 4},
+                                                    {0, 1, 0, 1},
+                                                    {1, 15, 0, 9},
+                                                    {1, 14, 2, 10},
+                                                    {1, 14, 0, 10},
+                                                    {1, 14, 3, 10},
+                                                    {1, 14, 1, 10},
+                                                    {1, 13, 2, 11},
+                                                    {1, 13, 1, 11},
+                                                    {1, 13, 0, 11},
+                                                    {1, 13, 3, 11},
+                                                    {2, 3, 0, 12},
+                                                    {2, 2, 0, 8},
+                                                    {2, 1, 0, 9},
+                                                    {1, 3, 2, 5},
+                                                    {1, 3, 0, 5},
+                                                    {1, 3, 3, 5},
+                                                    {1, 3, 1, 5},
+                                                    {2, 0, 2, 6},
+                                                    {2, 0, 1, 6},
+                                                    {2, 0, 0, 6},
+                                                    {2, 0, 3, 6}};
+
+unsigned int * const ports[3] = {&PORTA, &PORTB, &PORTC};
+unsigned int * const tris[3] = {&TRISA, &TRISB, &TRISC};
+
+unsigned int enabled = 0;
+unsigned int active_channel = 0;
+
+unsigned int base_stable = 0;
+unsigned int samples[TOUCH_CHANNEL_COUNT];
+unsigned int basecount[TOUCH_CHANNEL_COUNT];
+
+void touch_init(void) {
+    int i;
+
+    TMR1 = 0;                   // configure timer
+    PR1 = TOUCH_TIME_CONSTANT;
+
+    // CTMU configuration
+    _CTMUEN = 0;                // disable
+    _EDGSEQEN = 1;              // require two edges for current pulse
+    _EDG2POL = 1;               // rising edge
+    _EDG1POL = 1;
+    _EDG1SEL = 1;               // TODO: Is this right? Want to manually set this edge.
+    _CTTRIG = 1;                // trigger A/D conversion when current shuts off
+    _IRNG = 2;                  // 5.5 uA
+    _IDISSEN = 1;               // ground current source
+    _EDG2STAT = 0;              // clear edge status
+    _EDG1STAT = 0;
+
+    // ADC configuration
+    _SSRC = 6;                  // CTMU triggers conversion
+    _CH0SA = 1;                 // set default input to a grounded touch channel
+    _ADON = 0;
+
+    for (i = 0; i < TOUCH_CHANNEL_COUNT; i++) {
+        samples[i] = 0;
+        basecount[i] = 0;
+    }
+}
+
+void touch_enable(void) {
+    _CTMUEN = 1;
+    _EDG2STAT = 0;              // clear edge status
+    _EDG1STAT = 0;
+    _EDGEN = 1;                 // enabled edge inputs
+
+    _ADON = 1;
+    _AD1IF = 0;
+    _AD1IE = 1;
+    _AD1IP = 1;     // TODO: determine proper interrupt priority, should be low prio
+
+    enabled = 1;
+}
+
+void touch_next(void) {
+    unsigned int cpu_priority;
+    touch_channel prev_chan;
+    touch_channel cur_chan;
+
+    prev_chan = cmatrix[active_channel];
+
+    if (++active_channel >= TOUCH_CHANNEL_COUNT)
+        active_channel = 0;
+
+    cur_chan = cmatrix[active_channel];
+
+    RMBITW(*(tris[prev_chan.port]), prev_chan.pindex, 0);   // ground inactive channel
+    RMBITW(*(ports[prev_chan.port]), prev_chan.pindex, 0);  // TODO: shouldn't need this (value should always be 0).
+
+    TOUCH_AMUX_AMSEL0 = cur_chan.amux % 2;    // switch amux
+    TOUCH_AMUX_AMSEL1 = cur_chan.amux / 2;
+
+    _CH0SA = cur_chan.aindex;   // switch ADC channel
+    Nop(); Nop(); Nop();        // wait a little bit for ADC cap to discharge
+    RMBITW(*(tris[cur_chan.port]), cur_chan.pindex, 1);     // set pin to input
+
+    TMR1 = 0;       // reset timer count
+
+    _EDG1STAT = 0;  // clear CTMU triggers
+    _EDG2STAT = 0;
+
+    SET_AND_SAVE_CPU_IPL(cpu_priority, 7);  // disable interrupts for critical timing
+    _EDG1STAT = 1;  // enable current source
+    _TON = 1;       // enable timer
+    RESTORE_CPU_IPL(cpu_priority);          // re-enable interrupts
+}
+
+void touch_interval_delay(void) {
+    TMR1 = 0;
+    _TCKPS = 1;     // 8:1 prescaler
+    PR1 = TOUCH_SAMPLING_DELAY;
+
+    _T1IF = 0;
+    _T1IE = 1;
+    _TON = 1;
+}
+
+void touch_process_samples() {
+    
+}
+
+
+void _ISRFAST _ADC1Interrupt(void) {
+    _AD1IF = 0;
+
+    _TON = 0;   // TODO: this may need to be shut off in the timer interrupt
+
+    samples[active_channel] = ADC1BUF0;
+
+    if (active_channel == TOUCH_CHANNEL_COUNT - 1) {
+        touch_interval_delay();
+        touch_process_samples();
+    }
+    else {
+        touch_next();
+    }
+}
+
+void _ISRFAST _T1Interrupt(void) {
+    _T1IF = 0;
+
+    _TON = 0;   // disable timer
+    _T1IE = 0;  // diable interrupt
+
+    TMR1 = 0;   // reset time period for touch
+    PR1 = TOUCH_TIME_CONSTANT;
+    _TCKPS = 0;
+
+    touch_next();   // restart sample process
+}
---
+++ touch.h
@@ -0,0 +1,39 @@
+/* 
+ * File:   touch.h
+ * Author: Matt
+ *
+ * Created on January 29, 2013, 8:55 AM
+ */
+
+#ifndef TOUCH_H
+#define	TOUCH_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define RMBITW(var, pos, val) var = (var & ~(1 << pos)) | ((val % 2) << pos)
+
+#define TOUCH_CHANNEL_COUNT     22
+
+#define TOUCH_AMUX_AMSEL0       _RB8
+#define TOUCH_AMUX_AMSEL1       _RB9
+
+/** 20us: 320 counts @ 16Mhz FCY */
+#define TOUCH_TIME_CONSTANT     320
+
+#define TOUCH_SAMPLING_DELAY    19000
+
+#define TOUCH_DETECT_THRESHOLD  100
+
+void touch_init(void);
+void touch_enable(void);
+void touch_disable(void);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TOUCH_H */
+
